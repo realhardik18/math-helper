393b1d81fde48fd4187a9bf8fe9cf41f06bdae53
hardik singh <realhardik18@gmail.com>
2021-09-03 11:45:57 +0530

    made it in such a way that  it filters out the pdf and textbooks

--- googler.py
+++ googler.py
@@ -2,6 +2,9 @@ from googlesearch import search
 
 def search_google(question):
     results=[]
-    for j in search(question, tld="co.in", num=10, stop=10, pause=2):
-        results.append(j)
+    for j in search(question, tld="co.in", num=10, stop=None, pause=2):
+        if "pdf" in j.lower() or "textbook" in j.lower():
+            pass
+        else:
+            results.append(j)
     return results
