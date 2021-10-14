29b2033c2f1baa791c444e7d52490a67e58aa9d8
hardik singh <realhardik18@gmail.com>
2021-10-14 22:03:04 +0530

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -4,7 +4,7 @@ from pytesseract import pytesseract
 import os
 
 def extracter(file_name):
-    path_to_tesseract = "/app/.apt/usr/bin/tesseract"
+    path_to_tesseract = "TESSDATA_PREFIX"
     image_path = file_name
     img = Image.open(image_path)
     img_gray=ImageOps.grayscale(img)
