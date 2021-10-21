fa2753a5b6685f54af927618002bafc74c63c6ef
Techcube <qwertytothe1212@gmail.com>
2021-10-21 13:11:16 +0530

    Parse JSON, create loops
    Bug in loops (shows only last pod)

--- wolfram.py
+++ wolfram.py
@@ -1,5 +1,16 @@
-import requests, json
+import requests
 def wlfAlpha(text):
-    returned_json = requests.get(f'http://api.wolframalpha.com/v2/query?appid=4PJG6W-L4QUR22XJT&input={text}&output=json').json()
-    return json.dumps(returned_json)
+    returnedJson = requests.get(f'http://api.wolframalpha.com/v2/query?appid=4PJG6W-L4QUR22XJT&input={text}&output=json').json()
+    returnedPods = returnedJson['queryresult']['pods']
+    returnVar = {}
+    returnVar['images'] = []
+    returnVar['text'] = []
+    for i in range(len(returnedPods)):
+        for j in range(len(returnedPods[i]['subpods'])):
+            if returnedPods[i]['subpods'][j]['img']['title'] == "":
+                returnVar['images'].append(returnedPods[i]['subpods'][j]['img']['src'])
+            returnText = []
+            returnText.append(returnedPods[i]['title'] + "\n\t" + returnedPods[i]['subpods'][j]['plaintext'])
+            returnVar['text'] = returnText
+    return returnVar
 print(wlfAlpha('x*2=y^2'))
